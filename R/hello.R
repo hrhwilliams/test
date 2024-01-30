2c6f3fa57acab8f94fe14f6f4ed22f7ebb9d76b0
fiatlux20 <harlanrhwilliams@gmail.com>
2024-01-30 12:58:36 -0600

    add documentation to hello_world

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,11 @@
-hello_world <- function() {
-  print('hello world')
+#' hello_world
+#'
+#' @param name someone's name
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples hello_world('Harlan')
+hello_world <- function(name) {
+  print('hello, ', name)
 }
